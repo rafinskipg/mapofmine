f9280aa9477e1716d333c519accfb254e2ddb0e9
Rafael Pedrola <rafinskipg@gmail.com>
2014-12-29 19:15:54 +0100

    added home styles

--- public/js/signup.js
+++ public/js/signup.js
@@ -17,20 +17,31 @@ function submitForm(e){
     });
 }
 
-function initBindings(){
-  $('#signup').on('click', submitForm);
-  $('.email').on('keyup', resetMessages);
-}
-
 function resetMessages(){
   $('.error-message').addClass('hide').removeClass('animated fadeIn');
   $('.confirm').addClass('hide').removeClass('animated fadeIn');
 
   if($('.form .email').val().length > 0){
-    $('#signup').attr('disabled', false);
+    $('#signup-button').attr('disabled', false);
   }else{
-    $('#signup').attr('disabled', 'disabled');
+    $('#signup-button').attr('disabled', 'disabled');
   }
+}
+
+function initMenu(){
+  $('.page-scroll a').on('click', function(e){
+    e.preventDefault();
+    var target = $('#'+ $(e.currentTarget).attr('href').replace('/#', ''));
+     $('html,body').animate({
+        scrollTop: target.offset().top - 50
+      }, 500);
+  })
+}
+
+function initBindings(){
+  $('#signup-button').on('click', submitForm);
+  $('.email').on('keyup', resetMessages);
+  initMenu();
 }
 
 $(document).ready(initBindings);
